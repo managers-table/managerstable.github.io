9f279e5c1584031af3fdc57231947cfef759fd5e
Ryan Burgess <burgessdryan@gmail.com>
2024-01-03 10:28:07 -0800

    switch to Spotify hosting for feed

--- templates/episode-page.js
+++ templates/episode-page.js
@@ -4,7 +4,7 @@ module.exports = function episodePage(epDate, id, epDesc) {
   'use strict';
   return `<div class="container"><p>Published ${epDate}</p>
 <div class="audio">
-<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${id}&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
+<iframe src="https://podcasters.spotify.com/pod/show/ryan-burgess29/episodes/${id}/embed" height="100%" width="100%" frameborder="0" scrolling="no"></iframe>
 </div>
 <p>${epDesc}</p></div>`;
 };
