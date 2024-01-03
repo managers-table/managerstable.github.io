5d5e8ef193df120e20561f730f4f9a06467d1ceb
Ryan Burgess <burgessdryan@gmail.com>
2024-01-03 10:42:40 -0800

    update spotify iframe

--- templates/episode-page.js
+++ templates/episode-page.js
@@ -4,7 +4,7 @@ module.exports = function episodePage(epDate, id, epDesc) {
   'use strict';
   return `<div class="container"><p>Published ${epDate}</p>
 <div class="audio">
-<iframe src="https://podcasters.spotify.com/pod/show/ryan-burgess29/episodes/${id}/embed" height="100%" width="100%" frameborder="0" scrolling="no"></iframe>
+<iframe src="https://podcasters.spotify.com/pod/show/managers-table/episodes/${id}/embed" height="100%" width="100%" frameborder="0" scrolling="no"></iframe>
 </div>
 <p>${epDesc}</p></div>`;
 };
